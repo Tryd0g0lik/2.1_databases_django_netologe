1c7ff32d954a2907732a0bb6ff674251fe91375b
Tryd0g0lik <work80@mail.ru>
2022-10-02 11:56:56 +0700

    we got a run server

--- work_with_database/main/settings.py
+++ work_with_database/main/settings.py
@@ -32,7 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'management'
+    # 'management',
+    # 'phones.apps,AppConfig',
     'phones',
 ]
 
