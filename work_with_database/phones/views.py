dcc25a898bb2c0723c12cd1d31c13f0aee66cac2
Tryd0g0lik <work80@mail.ru>
2022-10-03 16:12:58 +0700

    it is can do sorting the action

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -10,13 +10,23 @@ def index(request):
 
 
 def show_catalog(request):
-    product_object = Phone.objects.all()
+    sort_name = request.GET.get('sort')
+    if sort_name == 'name': # , 'min_price', 'max_price']
+      product_object = Phone.objects.all().order_by('name')
+    elif sort_name == 'min_price':
+      product_object = Phone.objects.all().order_by('price')
+    elif sort_name == 'max_price':
+      product_object = Phone.objects.all().order_by('-price')
+    else:
+      product_object = Phone.objects.all()
+
     template = 'catalog.html'
     phones = [p for p in product_object]
 
     return render(request=request,
                   template_name=template,
                   context={'phones' : phones})
+
 
 
 def show_product(request, slug):
